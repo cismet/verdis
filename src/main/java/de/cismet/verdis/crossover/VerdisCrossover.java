e6cd988ec0af0916cf2b328015900129734065f0
Sebastian Puhl <sebastian.puhl@cismet.de>
2009-10-12 11:46:46 +0000

    fixed[Isssue:0001109]

--- src/main/java/de/cismet/verdis/crossover/VerdisCrossover.java
+++ src/main/java/de/cismet/verdis/crossover/VerdisCrossover.java
@@ -9,6 +9,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 
 /**
  *
@@ -19,11 +21,27 @@ public class VerdisCrossover {
 
     private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(VerdisCrossover.class);
 
+
+    
+    @Context private UriInfo context;
+
     @GET
     @Produces("text/html")
     @Path("/gotoKassenzeichen/")
     public String gotoKassenzeichen(@QueryParam("kassenzeichen") String kassenzeichen) {
-        log.debug("Crossover: gotoKassenzeichen");
+        log.debug("Crossover: gotoKassenzeichen");        
+        
+        try{
+           final String host = context.getBaseUri().getHost();
+           if(!host.equals("localhost")&&!host.equals("127.0.0.1")){
+               log.info("Keine Request von remote rechnern möglich: "+host);
+                return "<html>Es können nur Requests vom lokalen Rechner abgesetzt werden. Es kann nicht zum gewünschten Kassenzeichen gewechselt werden</html>";
+           }
+        }catch(Exception ex){
+            log.error("Fehler beim bestimmen des Hosts Request nicht möglich");
+            return "<html>Der Host konnte nicht bestimmt werden. Es kann nicht zum gewünschten Kassenzeichen gewechselt werden</html>";
+        }
+
         //Mit History
         if (Main.getCurrentInstance().isLoggedIn()) {
             try {
