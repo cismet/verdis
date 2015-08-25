1426c3134d6928419a2866aebdb3f8f722b10d49
JeanAtCismet <jean.ruiz@cismet.de>
2015-08-25 19:01:30 +0200

    issue #136

--- src/main/java/de/cismet/verdis/gui/PopupLagisCrossoverPanel.java
+++ src/main/java/de/cismet/verdis/gui/PopupLagisCrossoverPanel.java
@@ -50,6 +50,7 @@ import de.cismet.layout.FadingCardLayout;
 
 import de.cismet.verdis.CidsAppBackend;
 
+import de.cismet.verdis.commons.constants.GeomPropertyConstants;
 import de.cismet.verdis.commons.constants.KassenzeichenGeometriePropertyConstants;
 import de.cismet.verdis.commons.constants.KassenzeichenPropertyConstants;
 
@@ -676,7 +677,10 @@ public class PopupLagisCrossoverPanel extends javax.swing.JPanel implements Mous
 
                 final List<Geometry> geoms = new ArrayList<Geometry>();
                 for (final CidsBean kgeom : kgeoms) {
-                    geoms.add((Geometry)kgeom.getProperty(KassenzeichenGeometriePropertyConstants.PROP__GEOMETRIE));
+                    geoms.add((Geometry)kgeom.getProperty(
+                            KassenzeichenGeometriePropertyConstants.PROP__GEOMETRIE
+                                    + "."
+                                    + GeomPropertyConstants.PROP__GEO_FIELD));
                 }
 
                 if (!geoms.isEmpty()) {
