0cf60e2caa6036e85ca9b8f48960a0e4dd66dc39
JeanAtCismet <jean.ruiz@cismet.de>
2017-09-19 11:21:55 +0200

    issue #184 fixing disabled components not beeing enabled again

--- src/main/java/de/cismet/verdis/gui/srfronten/SRFrontenDetailsPanel.java
+++ src/main/java/de/cismet/verdis/gui/srfronten/SRFrontenDetailsPanel.java
@@ -820,7 +820,7 @@ public class SRFrontenDetailsPanel extends AbstractCidsBeanDetailsPanel {
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void cboStrasseActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_cboStrasseActionPerformed
+    private void cboStrasseActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboStrasseActionPerformed
         CidsBean strasseBean = null;
         if (frontBean != null) {
             strasseBean = (CidsBean)frontBean.getProperty(FrontPropertyConstants.PROP__FRONTINFO + "."
@@ -882,14 +882,14 @@ public class SRFrontenDetailsPanel extends AbstractCidsBeanDetailsPanel {
             dcmSR.addElement(null);
             cboLageSR.setModel(dcmSR);
         }
-    } //GEN-LAST:event_cboStrasseActionPerformed
+    }//GEN-LAST:event_cboStrasseActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void cboLageSRActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_cboLageSRActionPerformed
+    private void cboLageSRActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboLageSRActionPerformed
         try {
             final CidsBean oldLageSRBean = (CidsBean)getCidsBean().getProperty(FrontPropertyConstants.PROP__FRONTINFO
                             + "."
@@ -916,14 +916,14 @@ public class SRFrontenDetailsPanel extends AbstractCidsBeanDetailsPanel {
             LOG.warn(ex, ex);
         }
         updateLageSrCbo();
-    } //GEN-LAST:event_cboLageSRActionPerformed
+    }//GEN-LAST:event_cboLageSRActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void cboSRActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_cboSRActionPerformed
+    private void cboSRActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboSRActionPerformed
         if (getCidsBean() != null) {
             try {
                 final CidsBean lageSRBean = (CidsBean)getCidsBean().getProperty(FrontPropertyConstants.PROP__FRONTINFO
@@ -941,7 +941,7 @@ public class SRFrontenDetailsPanel extends AbstractCidsBeanDetailsPanel {
             }
             updateLageSrCbo();
         }
-    }                                                                         //GEN-LAST:event_cboSRActionPerformed
+    }//GEN-LAST:event_cboSRActionPerformed
 
     /**
      * DOCUMENT ME!
@@ -1152,12 +1152,19 @@ public class SRFrontenDetailsPanel extends AbstractCidsBeanDetailsPanel {
     public void setEnabled(final boolean bln) {
         super.setEnabled(bln);
         txtBearbeitetDurch.setEditable(bln);
+        txtBearbeitetDurch.setEnabled(true);
         txtBemSR.setEditable(bln);
+        txtBemSR.setEnabled(true);
         txtErfassungsdatum.setEditable(bln);
+        txtErfassungsdatum.setEnabled(true);
         txtLaengeGrafik.setEditable(bln);
+        txtLaengeGrafik.setEnabled(true);
         txtLaengeKorrektur.setEditable(bln);
+        txtLaengeKorrektur.setEnabled(true);
         txtNummer.setEditable(bln);
+        txtNummer.setEnabled(true);
         txtVeranlagungSR.setEditable(bln);
+        txtVeranlagungSR.setEnabled(true);
         cboSR.setEnabled(bln);
         cboStrasse.setEnabled(bln);
         cboLageSR.setEnabled(bln);
