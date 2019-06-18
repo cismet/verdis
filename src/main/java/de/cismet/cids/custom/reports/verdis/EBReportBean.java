7c835a04ecd9f52bc7a53828c72b35ef68dd9fca
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2019-06-18 17:30:06 +0200

    fixed #214

--- src/main/java/de/cismet/cids/custom/reports/verdis/EBReportBean.java
+++ src/main/java/de/cismet/cids/custom/reports/verdis/EBReportBean.java
@@ -11,6 +11,9 @@
  */
 package de.cismet.cids.custom.reports.verdis;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import org.apache.log4j.Logger;
 
 import java.awt.Image;
@@ -27,6 +30,8 @@ import de.cismet.verdis.commons.constants.KassenzeichenPropertyConstants;
  * @author   daniel
  * @version  $Revision$, $Date$
  */
+@Getter
+@Setter
 public abstract class EBReportBean {
 
     //~ Static fields/initializers ---------------------------------------------
@@ -42,6 +47,7 @@ public abstract class EBReportBean {
     private Image barcodeImage = null;
     private boolean fillAbflusswirksamkeit = false;
     private String mapHint = null;
+    private String bearbeiterKuerzel;
     private final Properties properties;
 
     //~ Constructors -----------------------------------------------------------
@@ -59,6 +65,18 @@ public abstract class EBReportBean {
         kznr = "" + (Integer)this.kassenzeichen.getProperty(
                         KassenzeichenPropertyConstants.PROP__KASSENZEICHENNUMMER);
         this.fillAbflusswirksamkeit = fillAbfluss;
+        final String letzteAenderungVon = (String)this.kassenzeichen.getProperty("letzte_aenderung_von");
+        if ((letzteAenderungVon != null) && !letzteAenderungVon.isEmpty()) {
+            this.bearbeiterKuerzel = letzteAenderungVon.split("@")[0].substring(0, 3);
+            for (int i = 1; i < letzteAenderungVon.length(); i++) {
+                if (Character.isUpperCase(letzteAenderungVon.charAt(i))) {
+                    bearbeiterKuerzel += letzteAenderungVon.charAt(i);
+                    break;
+                }
+            }
+        } else {
+            this.bearbeiterKuerzel = null;
+        }
     }
 
     //~ Methods ----------------------------------------------------------------
@@ -68,142 +86,7 @@ public abstract class EBReportBean {
      *
      * @return  DOCUMENT ME!
      */
-    public String getMapHint() {
-        return mapHint;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param  mapHint  DOCUMENT ME!
-     */
-    public void setMapHint(final String mapHint) {
-        this.mapHint = mapHint;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    public boolean isFillAbflusswirksamkeit() {
-        return fillAbflusswirksamkeit;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param  fillAbflusswirksamkeit  DOCUMENT ME!
-     */
-    public void setFillAbflusswirksamkeit(final boolean fillAbflusswirksamkeit) {
-        this.fillAbflusswirksamkeit = fillAbflusswirksamkeit;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    public CidsBean getKassenzeichenBean() {
-        return kassenzeichen;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param  feb  DOCUMENT ME!
-     */
-    public void setKassenzeichenBean(final CidsBean feb) {
-        this.kassenzeichen = feb;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    public String getKznr() {
-        return kznr;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param  kassenzeichennummer  DOCUMENT ME!
-     */
-    public void setKznr(final String kassenzeichennummer) {
-        this.kznr = kassenzeichennummer;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    public String getScale() {
-        return scale;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param  scale  DOCUMENT ME!
-     */
-    public void setScale(final String scale) {
-        this.scale = scale;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    public Image getMapImage() {
-        return mapImage;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param  mapImage  DOCUMENT ME!
-     */
-    public void setMapImage(final Image mapImage) {
-        this.mapImage = mapImage;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
     public boolean isReadyToProceed() {
-        return (mapImage != null) && (kassenzeichen != null);
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    public Properties getProperties() {
-        return properties;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    public Image getBarcodeImage() {
-        return barcodeImage;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param  barcodeImage  DOCUMENT ME!
-     */
-    public void setBarcodeImage(final Image barcodeImage) {
-        this.barcodeImage = barcodeImage;
+        return (mapImage != null);
     }
 }
